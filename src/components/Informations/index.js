904d07b03a29451942f84b5cdb227641fb253055
Matthias <matthias.gander@braineet.com>
2021-02-10 10:20:20 +0100

    new game

--- src/components/Informations/index.js
+++ src/components/Informations/index.js
@@ -6,11 +6,14 @@ const Informations = ({ mapHover }) => {
   console.log(texts);
   return (
     <div>
-      <div className="title">{texts[mapHover]}</div>
+      <div className="title">{texts[mapHover].title}</div>
+      <div className="description">{texts[mapHover].description}</div>
     </div>
   );
 };
 
-Informations.propTypes = {};
+Informations.propTypes = {
+  mapHover: PropTypes.string.isRequired,
+};
 
 export default Informations;
